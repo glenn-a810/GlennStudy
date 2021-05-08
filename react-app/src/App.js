fcc3a6e2936ea0fccff3585e596a24ee31baabe3
Glenn <a810.glenn@gmail.com>
2021-05-09 01:56:21 +0900

    Component 만들기라고 했지만 이건 Component가 아닌거 같아..

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component {
+  render(){
+    return(
+      <header>
+        <h1>WEB</h1>
+        HTML5 Simentic 형식으로 일단 표현
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return(
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML은 프로그래밍 언어가 아닙니다!
+      </article>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
+      </div>
+    )
+  }
 }
 
 export default App;
