3f028be5308bbfe6daf5089b69153012649d66a6
Glenn <a810.glenn@gmail.com>
2021-05-10 19:46:36 +0900

    state랑 key 겉핥기 중..

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,11 +5,29 @@ import Subject from "./components/Subject";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'welcome', 
+      subject:{title:'REACT', sub:'For UI'},
+      welcome:{title:'Welcome', desc:'Hello, React'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      ]
+    }
+  }
+
   render() {
+    if(this.state.mode)
     return (
       <div className="App">
-        <Subject title="REACT" sub="For UI"></Subject>
-        <TOC></TOC>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML은 프로그래밍 언어가 아닙니다!"></Content>
       </div>
     )
--- react-app/src/components/Subject.js
+++ react-app/src/components/Subject.js
@@ -4,7 +4,7 @@ class Subject extends Component {
     render () {
         return (
             <header>
-                <h1>{this.props.title}</h1>
+                <h1><a href="/">{this.props.title}</a></h1>
                 {this.props.sub}
             </header>
         )
