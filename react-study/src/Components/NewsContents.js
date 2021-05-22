11f41fe18337cc133341ad8e2cdf001c50f4950a
Glenn <a810.glenn@gmail.com>
2021-05-23 02:45:42 +0900

    관우 과제 : 맨 마지막 기사부터 삭제하기(수정)

--- react-study/src/Components/NewsContents.js
+++ react-study/src/Components/NewsContents.js
@@ -6,7 +6,7 @@ function NewsContents({index, title, description, author, newsDelete}) {
             <h3>{title}</h3>
             <h5>{description}</h5>
             <h5>{author}</h5>
-            <button onClick={newsDelete}>삭제</button>
+            {/* <button onClick={newsDelete}>삭제</button> */}
             <hr/>
         </div>
     )
