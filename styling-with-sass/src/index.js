2dacafb7d5091b962e36fa464b11a56a04bdaaa8
Glenn <a810.glenn@gmail.com>
2021-05-29 02:22:31 +0900

    sass 기본 세팅 + 테스트

--- styling-with-sass/src/index.js
+++ styling-with-sass/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Counter from './Components/Counter'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-// ReactDOM.render(<Counter />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
