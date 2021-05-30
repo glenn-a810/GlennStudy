009848914f5789344b082bb5cc3b1bf69f76e231
Glenn <a810.glenn@gmail.com>
2021-05-30 16:47:48 +0900

    scss 사용법 및 classNames 패키지 사용법 연습

--- styling-with-sass/src/Components/Button.js
+++ styling-with-sass/src/Components/Button.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import classNames from 'classnames'
 import './Button.scss'
 
-function Button({children}) {
-    return <button className='Button'>{children}</button>
+function Button({children, size, color, outline, fullWidth, ...rest}) {
+    return <button 
+        className={classNames('Button', size, color, {outline, fullWidth})}
+        {...rest}>
+            {children}
+        </button>
+}
+
+Button.defaultProps = {
+    size: 'medium',
+    color: 'blue'
 }
 
 export default Button
